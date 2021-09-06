f3118256b14a7be0981a660b6ad4d6abc608b590
nstrong <nathanstrong.fx@gmail.com>
2021-09-06 13:15:50 -0700

    init

--- src/components/UI/ErrorModal.tsx
+++ src/components/UI/ErrorModal.tsx
@@ -1,6 +1,7 @@
 import style from './ErrorModule.module.css';
 import Card from './Card';
 import Button from './Button';
+import {createPortal} from 'react-dom';
 import {MouseEventHandler, ReactElement} from 'react';
 
 export interface IError {
@@ -10,10 +11,13 @@ export interface IError {
     onConfirmed: MouseEventHandler<HTMLDivElement>;
 }
 
-function ErrorModal(props: IError) {
-    return <>
-        <div className={style.backdrop}
-             onClick={props.onConfirmed}/>
+function Backdrop(props: any) {
+    return <div className={style.backdrop}
+                onClick={props.onConfirmed}/>;
+}
+
+function ModalOverlay(props: any) {
+    return (
         <Card className={style.modal}>
 
             <header className={style.header}>
@@ -31,6 +35,13 @@ function ErrorModal(props: IError) {
                 <Button onClick={props.onConfirmed}>Okay</Button>
             </footer>
         </Card>
+    );
+}
+
+function ErrorModal(props: IError) {
+    return <>
+        {createPortal(<Backdrop {...props}/>, document.getElementById('backdrop-root') as HTMLElement)}
+        {createPortal(<ModalOverlay {...props}/>, document.getElementById('overlay-root') as HTMLElement)}
     </>;
 }
 
